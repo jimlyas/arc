8cc116883b9be8dd8656074f1987f29fb961ff3e
jimlyas <gmail@jimlyas.com>
2022-09-16 15:04:28 +0700

    refactor:  - organize version catalog alphabetically  - add license to the project

---
+++ modules/presentation/src/main/java/jimlyas/arc/presentation/activity/ArcActivity.kt
@@ -0,0 +1,13 @@
+package jimlyas.arc.presentation.activity
+
+import androidx.core.app.ActivityCompat
+
+/**
+ * @author jimlyas
+ * @since 16/09/22
+ *
+ * Copyright © 2022 jimlyas. All rights reserved.
+ */
+class ArcActivity : ActivityCompat() {
+
+}
